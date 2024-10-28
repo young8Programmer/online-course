f36c12fda23a1ca67c2d77367590b64548f125b0
Vali <valireyimbergenov79@gmail.com>
2024-10-28 20:32:23 +0500

    package typeorm and moduls add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AssignmentsModule } from './assignments/assignments.module';
+import { CoursesModule } from './courses/courses.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { ModuleModule } from './module/module.module';
+import { ResultsModule } from './results/results.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, AssignmentsModule, CoursesModule, LessonsModule, ModuleModule, ResultsModule],
   controllers: [AppController],
   providers: [AppService],
 })
