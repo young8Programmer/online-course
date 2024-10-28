e0437363400c282f64f329aec739647c83320da0
Vali <valireyimbergenov79@gmail.com>
2024-10-28 23:08:17 +0500

    add TypeORM integration and modules setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { AssignmentsModule } from './assignments/assignments.module';
 import { CoursesModule } from './courses/courses.module';
-import { LessonsModule } from './lessons/lessons.module';
 import { ModuleModule } from './module/module.module';
+import { LessonsModule } from './lessons/lessons.module';
+import { AssignmentsModule } from './assignments/assignments.module';
 import { ResultsModule } from './results/results.module';
 
 @Module({
-  imports: [AuthModule, AssignmentsModule, CoursesModule, LessonsModule, ModuleModule, ResultsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "1234",
+      database: "online_course",
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+    AuthModule,
+    CoursesModule,
+    ModuleModule,
+    LessonsModule,
+    AssignmentsModule,
+    ResultsModule,
+  ]
 })
 export class AppModule {}
